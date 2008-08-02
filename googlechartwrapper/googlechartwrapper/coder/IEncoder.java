96680427f95ff1ff3b0b4fa433f220aa42d254d8
steffan3 <steffan3@example.com>
2008-08-02 21:02:05 +0000

    git-svn-id: http://googlechartwrapper.googlecode.com/svn/trunk@2 87d38b24-3e2c-11de-9b3e-8f53452658d7

---
+++ googlechartwrapper/googlechartwrapper/coder/IEncoder.java
@@ -0,0 +1,42 @@
+package googlechartwrapper.coder;
+
+import java.util.Collection;
+
+/**
+ * The Interface for the Encoder. The Encoder defines the way how your data will be encoded.<br />
+ * To receive more information about the encoding kinds study <a href=" die url">http://code.google.com/apis/chart/#text</a>.
+ * 
+ * @author steffan
+ *
+ */
+public interface IEncoder {
+	
+	/**
+	 * Encodes the given argument.
+	 * @param values array of integers to encode
+	 * @return encoded string
+	 */
+	public String encode(int[] values);
+	
+	/**
+	 * Encodes the given argument.
+	 * @param values array of float to encode
+	 * @return encoded string
+	 */
+	public String encode(float[] values);
+	
+	/**
+	 * Encodes the given argument.
+	 * @param values collection of integer arrays 
+	 * @return encoded string
+	 */
+	public String encodeIntegerCollection(Collection<int[]> values);
+	
+	/**
+	 * Encodes the given argument.
+	 * @param values collection of float arrays
+	 * @return encoded string
+	 */
+	public String encodeFloatCollection(Collection<float[]> values);	
+
+}
