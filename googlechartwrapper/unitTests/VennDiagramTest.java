a368e3d40aaba6cb2742c1df60092e207efe8135
martram <martram@example.com>
2008-10-05 15:55:09 +0000

    modified unittest to reflect changes made in the api
    git-svn-id: http://googlechartwrapper.googlecode.com/svn/trunk@71 87d38b24-3e2c-11de-9b3e-8f53452658d7

--- googlechartwrapper/unitTests/VennDiagramTest.java
+++ googlechartwrapper/unitTests/VennDiagramTest.java
@@ -6,7 +6,7 @@ import googlechartwrapper.color.LinearGradient;
 import googlechartwrapper.color.LinearStripes;
 import googlechartwrapper.color.LinearGradient.GradientFillDestination;
 import googlechartwrapper.color.LinearStripes.LinearStripesDestination;
-import googlechartwrapper.data.DataScaling;
+import googlechartwrapper.data.DataScalingSet;
 import googlechartwrapper.data.VennDiagramData;
 import googlechartwrapper.label.ChartLegend;
 import googlechartwrapper.label.ChartTitle;
@@ -48,7 +48,7 @@ public class VennDiagramTest {
 		diagram.setChartLegend(new ChartLegend(l));
 
 		//System.out.println(diagram.getUrl());
-		String target = "http://chart.apis.google.com/chart?cht=v&chs=200x200&chtt=VennDiagramm&chco=00ff00,0000ff,ff0000&chd=e:BkBQA8AeAeAeAK&chdl=A|C|V";
+		String target = "http://chart.apis.google.com/chart?cht=v&chs=200x200&chdl=A|C|V&chtt=VennDiagramm&chd=e:..zMmZTNTNTNGa&chco=00ff00,0000ff,ff0000";
 		
 		Assert.assertEquals(target, diagram.getUrl());
 	}
@@ -67,7 +67,7 @@ public class VennDiagramTest {
 		diagram.addChartColor(new ChartColors(Color.BLUE));
 		diagram.addChartColor(new ChartColors(Color.RED));
 
-		diagram.setDataScaling(new DataScaling(10,50));
+		diagram.setDataScaling(new DataScalingSet(10,50));
 
 		diagram.setLinearGradient(new LinearGradient(
 				GradientFillDestination.Background, 10, Color.BLACK, 0.5f,
@@ -81,7 +81,7 @@ public class VennDiagramTest {
 		diagram.setChartLegend(new ChartLegend(l));
 
 		//System.out.println(diagram.getUrl());
-		String target = "http://chart.apis.google.com/chart?cht=v&chs=200x200&chf=bg,lg,10,000000,0.5,ff0000,1.0&chtt=VennDiagramm&chco=00ff00,0000ff,ff0000&chds=10,50&chd=t:100,80,60,30,30,30,1&chdl=A|C|V";
+		String target = "http://chart.apis.google.com/chart?cht=v&chs=200x200&chdl=A|C|V&chf=bg,lg,10,000000,0.5,ff0000,1.0&chtt=VennDiagramm&chds=10.0,50.0&chd=t:100,80,60,30,30,30,1&chco=00ff00,0000ff,ff0000";
 
 		
 		Assert.assertEquals(target, diagram.getUrl());
@@ -110,7 +110,7 @@ public class VennDiagramTest {
 				Color.LIGHT_GRAY, 1f));
 
 		//System.out.println(diagram.getUrl());
-		String target = "http://chart.apis.google.com/chart?cht=v&chs=200x200&chf=bg,ls,1,00ffff,0.5,c0c0c0,1.0&chtt=VennDiagramm&chd=e:BaBGAUAKAFAFAK&chdl=A|C|V";
+		String target = "http://chart.apis.google.com/chart?cht=v&chs=200x200&chdl=A|C|V&chf=bg,ls,1,00ffff,0.5,c0c0c0,1.0&chtt=VennDiagramm&chd=e:5mszMzGaDNDNGa";
 
 		
 		Assert.assertEquals(target, diagram.getUrl());
@@ -126,14 +126,14 @@ public class VennDiagramTest {
 		diagram.setVennDiagramData(new VennDiagramData(90, 70, 20, 10, 5, 5,
 				10));
 		
-		diagram.setDataScaling(new DataScaling(10,50));
+		diagram.setDataScaling(new DataScalingSet(10,50));
 		
 		diagram.setVennDiagramData(new VennDiagramData(100, 80, 60, 30, 30, 30,
 				10));
 		
 
 		//System.out.println(diagram.getUrl());
-		String target = "http://chart.apis.google.com/chart?cht=v&chs=200x200&chtt=VennDiagramm&chds=10,50&chd=t:100,80,60,30,30,30,1";
+		String target = "http://chart.apis.google.com/chart?cht=v&chs=200x200&chtt=VennDiagramm&chds=10.0,50.0&chd=t:100,80,60,30,30,30,1";
 		
 		Assert.assertEquals(target, diagram.getUrl());
 	}
